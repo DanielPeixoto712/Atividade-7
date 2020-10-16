6a0d4dd0b5ebec850cbd50d987d2de88a5a219ea
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 12:23:10 +0100

    Update 1.php, 2.php, and 2 more files...

---
+++ 4.php
@@ -0,0 +1,28 @@
+<?php
+$numero = 50;
+switch ($numero) {
+	case ($numero):
+		echo 'Valor Muito Alto';
+		break;
+
+	case ($numero <80 && $numero):
+			echo 'Valor médio';
+			break;	
+
+	case 'numero === 50':
+				echo 'Valor Perfeito';
+				break;
+
+	case 'numero <= 10':
+				echo 'Valor muito baixo';
+				break;
+
+	case 'numero == 0':
+				echo 'Sem Valor';
+				break;						
+
+
+	
+
+}
+?>
